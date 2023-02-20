5cd09bba939642ca0d1f6f49d0dc0e242ed60f81
Leejunho <wpsxmfwnsgh205@naver.com>
2023-02-20 17:56:20 +0900

    2023년 2월20일

---
+++ Java/src/basic/CastingEXample1.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class CastingEXample1 {
+
+	public static void main(String[] args) {
+		
+        /*
+         * 크기가 작은 데이터 타입의 자료를 큰 데이터 타입으로 변환할 때는
+         * 자바 가상 머신(jav) 이 자동으로 타입을 올려서 변환해 줍니다.
+         * (promotion , upcasting)
+         */
+		
+		byte b = 10;
+		int i = b;
+		System.out.println(i);
+		
+		char c = 'A';
+		int j = c;
+		System.out.println("A의문자 번호: " + j);
+		
+		int k = 500;
+		double d = k;
+		System.out.println(d);
+		
+		
+		
+	}
+
+}
